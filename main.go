55714baf29959646b94d048b078bf64b12e20bc4
Josh Hartwig <joshman211@gmail.com>
2024-09-23 20:17:37 -0500

    added concurrency, probably broke it though, fixed mutex for pagemap

--- main.go
+++ main.go
@@ -6,7 +6,18 @@ import (
 	"net/http"
 	"net/url"
 	"os"
+	"sync"
 )
+
+//TODO: figure out the channel portion we have the mutex part done
+
+type config struct {
+	pages              map[string]int
+	baseUrl            *url.URL
+	mu                 *sync.Mutex
+	concurrencyControl chan struct{}
+	wg                 *sync.WaitGroup
+}
 
 func main() {
 	args := os.Args
@@ -21,14 +32,25 @@ func main() {
 		os.Exit(1)
 	}
 
-	baseUrl := os.Args[1]
-	fmt.Printf("starting crawl\n%s\n", baseUrl)
+	baseUrl, err := url.Parse(os.Args[1]) //os.Args[1]
+	if err != nil {
+		fmt.Println("error parsing url:", os.Args[1])
+	}
 
-	// map to hold [url]count
+	fmt.Printf("starting crawl\n%s\n", baseUrl.String())
+
 	pages := map[string]int{}
 
-	crawlPage(baseUrl, baseUrl, pages)
+	cfg := config{
+		pages:              map[string]int{},
+		baseUrl:            baseUrl,
+		mu:                 &sync.Mutex{},
+		concurrencyControl: make(chan struct{}, 1),
+		wg:                 &sync.WaitGroup{},
+	}
 
+	go cfg.crawlPage(baseUrl.String())
+	cfg.wg.Wait()
 	fmt.Println(pages)
 }
 
@@ -59,63 +81,65 @@ func getHTML(rawURL string) (string, error) {
 }
 
 // recursively crawls a page getting urls from page and incrementing if we found existing
-func crawlPage(baseURL, currentURL string, pages map[string]int) {
-	// get the host name for both baseurl & current url and check if they are the same ex google.com
-	parsedBaseURL, err := url.Parse(baseURL)
-	if err != nil {
-		fmt.Println("error parsing url", baseURL, err)
-		return
-	}
+func (cfg *config) crawlPage(currentURL string) {
+	cfg.wg.Add(1)
+	defer cfg.wg.Done()
+
+	cfg.concurrencyControl <- struct{}{} // write to the channel
 
 	parsedCurrentURL, err := url.Parse(currentURL)
 	if err != nil {
-		fmt.Println("error parsing url", currentURL, err)
 		return
 	}
-	fmt.Printf("Is baseurl host: %s the same as currenturl host: %s \n", parsedBaseURL.Host, parsedCurrentURL.Host)
+
 	// if we are not on the same hostname return, do not crawl the entire internet only urls from host
-	if parsedBaseURL.Host != parsedCurrentURL.Host {
-		fmt.Printf("Returning baseurl: %s and currenturl: %s are the same hosts \n", parsedBaseURL, parsedCurrentURL)
+	if cfg.baseUrl.Host != parsedCurrentURL.Host {
 		return
 	}
 
 	// normalize the currentURL
-
 	normalizedCurrentURL, err := normalizeURL(currentURL)
 	if err != nil {
 		fmt.Println("error normalizing url", err)
 		return
 	}
-	fmt.Printf("normalizing current url: %s is normalized to: %s \n", currentURL, normalizedCurrentURL)
 
-	// check if the normalized current URL is in the map, if so increment if not add it
-	if _, ok := pages[normalizedCurrentURL]; !ok {
-		pages[normalizedCurrentURL] = 1
-		fmt.Printf("No entry in map for: %s setting value to 1 \n", normalizedCurrentURL)
-	} else {
-		pages[normalizedCurrentURL]++
-		fmt.Printf("Entry in map found for: %s incrementing value \n", normalizedCurrentURL)
-		return
+	if cfg.addPageVisit(normalizedCurrentURL) {
+		// we have not crawled the page, so fetch html
+		html, err := getHTML(currentURL)
+		if err != nil {
+			fmt.Println("error getting html", err)
+			return
+		}
+		fmt.Printf("Fetched html with size: %b from %s\n", len([]byte(html)), currentURL)
+
+		// get urls from html
+		urls, err := getURLsFromHTML(html, currentURL)
+		if err != nil {
+			fmt.Println("error getting urls")
+			return
+		}
+
+		// iterate through the urls and crawl
+		for _, url := range urls {
+			<-cfg.concurrencyControl // pull off the struct from the channel?
+			cfg.crawlPage(url)
+		}
+
 	}
 
-	// we have not crawled the page, so fetch html
-	fmt.Printf("Fetching url: %s \n", currentURL)
-	html, err := getHTML(currentURL)
-	if err != nil {
-		fmt.Println("error getting html", err)
-		return
-	}
-	fmt.Printf("Fetched html with size: %b from %s\n", len([]byte(html)), currentURL)
+	<-cfg.concurrencyControl
 
-	// get urls from html
-	urls, err := getURLsFromHTML(html, currentURL)
-	if err != nil {
-		fmt.Println("error getting urls")
-		return
-	}
+}
 
-	// iterate through the urls and crawl
-	for _, url := range urls {
-		crawlPage(baseURL, url, pages)
+func (c *config) addPageVisit(normalizedUrl string) (isFirst bool) {
+	c.mu.Lock()
+	if _, ok := c.pages[normalizedUrl]; !ok {
+		c.pages[normalizedUrl] = 1
+		c.mu.Unlock()
+		return isFirst
 	}
+	c.pages[normalizedUrl]++
+	c.mu.Unlock()
+	return !isFirst
 }
